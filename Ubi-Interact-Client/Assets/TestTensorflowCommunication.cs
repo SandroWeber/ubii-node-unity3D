ee6616e6d7ad4e096d02e30d09a1b25349351163
Redwerefox <lukas-goll@web.de>
2020-07-08 21:44:55 +0200

    push recent testing

--- Ubi-Interact-Client/Assets/TestTensorflowCommunication.cs
+++ Ubi-Interact-Client/Assets/TestTensorflowCommunication.cs
@@ -10,15 +10,17 @@ public class TestTensorflowCommunication : MonoBehaviour
 
     private UbiiClient ubiiClient = null;
     private string deviceName = "TestTensorflowCommunication - Device";
-    private string interactionID = "02bc02ea-8228-43cf-8d80-88f8923e912a"; 
+    private string interactionID = "02bc02ea-8228-43cf-8d80-88f8923e912a";
     private string topicTestSubscribe = null;
     private string topicTestPublish = null;
     private Ubii.Devices.Device ubiiDevice = null;
     private Ubii.Sessions.Session ubiiSession = null;
     private Ubii.Interactions.Interaction ubiiInteraction = null;
     private Ubii.Sessions.IOMapping IOMapping = null;
+    private Ubii.DataStructure.Object2DList predictions = null;
     Ubii.Sessions.InteractionInputMapping inputMapping = null;
     Ubii.Sessions.InteractionOutputMapping outputMapping = null;
+    double x = 0.0f;
 
     //debugging
     Ubii.Services.ServiceReply subscriptionReply = null;
@@ -27,8 +29,9 @@ public class TestTensorflowCommunication : MonoBehaviour
     private bool testRunning = false;
     private float tLastPublish = 0f;
 
-    private Vector3 outPublish = new Vector3(1,1,1);
-    private Vector3 testPosition = new Vector3(0,0,0);
+    private Vector3 outPublish = new Vector3(1, 1, 1);
+    private Vector3 testPosition;
+    private Ubii.DataStructure.Object2DList publishList;
 
     // Start is called before the first frame update
     void Start()
@@ -44,16 +47,21 @@ public class TestTensorflowCommunication : MonoBehaviour
         float tNow = Time.time;
         if (testRunning && tNow > tLastPublish + 1)
         {
-            ubiiClient.Publish(
-                new Ubii.TopicData.TopicData
+            Ubii.TopicData.TopicData publishdata = new Ubii.TopicData.TopicData
+            {
+                TopicDataRecord = new Ubii.TopicData.TopicDataRecord
                 {
-                    TopicDataRecord = new Ubii.TopicData.TopicDataRecord
-                    {
-                        Topic = ubiiDevice.Components[0].Topic,
-                        Vector3 = new Ubii.DataStructure.Vector3 { X = outPublish.x, Y = outPublish.y , Z = outPublish.z }
-                    }
-                });
-            Debug.Log(testPosition);
+                    Topic = topicTestPublish,
+                    Double =  4.0
+                }
+            };
+            ubiiClient.Publish(publishdata);
+            if(predictions != null)
+                Debug.Log(predictions);
+            else if( testPosition != null )
+                Debug.Log(testPosition);
+            else
+                Debug.Log(x);
             tLastPublish = tNow;
         }
     }
@@ -76,6 +84,19 @@ public class TestTensorflowCommunication : MonoBehaviour
                 Topic = UbiiConstants.Instance.DEFAULT_TOPICS.SERVICES.DEVICE_DEREGISTRATION,
                 Device = ubiiDevice
             });
+        }
+    }
+
+    async private void OnApplicationQuit()
+    {
+        if (ubiiSession != null)
+        {
+            /*
+            await ubiiClient.CallService(new Ubii.Services.ServiceRequest
+            {
+                Topic = UbiiConstants.Instance.DEFAULT_TOPICS.SERVICES.SESSION_STOP,
+                Session = ubiiSession
+            });*/
         }
     }
 
@@ -103,19 +124,19 @@ public class TestTensorflowCommunication : MonoBehaviour
             ubiiDevice = deviceRegistrationReply.Device;
         }
 
-        this.ubiiInteraction = new Ubii.Interactions.Interaction
+        /*this.ubiiInteraction = new Ubii.Interactions.Interaction
         {
-            Id = "Unity3D-Client-TestSessions-Interaction-01",
-            Name = "Unity3D-Client-TestSessions-Interaction-01",
-            ProcessingCallback = "(inputs, outputs, state) => { outputs.outVec3 = inputs.inVec3; }",
+            Id = "Unity3D-Client-TestSessions-Interaction-02",
+            Name = "Unity3D-Client-TestSessions-Interaction-02",
+            ProcessingCallback = "(inputs, outputs, state) => { console.log(inputs.inVec3);",
             ProcessFrequency = 10
         };
-        this.ubiiInteraction.InputFormats.Add(new Ubii.Interactions.IOFormat { InternalName = "inVec3", MessageFormat = "ubii.dataStructure.Vector3" });
-        this.ubiiInteraction.OutputFormats.Add(new Ubii.Interactions.IOFormat { InternalName = "outVec3", MessageFormat = "ubii.dataStructure.Vector3" });
-        this.ubiiInteraction.Authors.Add("Sandro Weber");
-        
+        this.ubiiInteraction.InputFormats.Add(new Ubii.Interactions.IOFormat { InternalName = "inVec3", MessageFormat = "vector3" });
+        this.ubiiInteraction.OutputFormats.Add(new Ubii.Interactions.IOFormat { InternalName = "outVec3", MessageFormat = "vector3" });
+        this.ubiiInteraction.Authors.Add("Lukas Goll");
+        */
 
-        /*Ubii.Services.ServiceReply interactionReply = await ubiiClient.CallService(new Ubii.Services.ServiceRequest
+        Ubii.Services.ServiceReply interactionReply = await ubiiClient.CallService(new Ubii.Services.ServiceRequest
         {
             Topic = UbiiConstants.Instance.DEFAULT_TOPICS.SERVICES.INTERACTION_DATABASE_GET,
             Interaction = new Ubii.Interactions.Interaction { Id = interactionID}
@@ -123,7 +144,7 @@ public class TestTensorflowCommunication : MonoBehaviour
         if (interactionReply.Interaction != null)
         {
             ubiiInteraction = interactionReply.Interaction;
-        }*/
+        }
 
         CreateUbiiSession();
         Ubii.Services.ServiceReply sessionRequest = await ubiiClient.CallService(new Ubii.Services.ServiceRequest
@@ -136,9 +157,11 @@ public class TestTensorflowCommunication : MonoBehaviour
             ubiiSession = sessionRequest.Session;
         }
 
-        await ubiiClient.Subscribe(ubiiDevice.Components[1].Topic, (Ubii.TopicData.TopicDataRecord record) =>
+        Ubii.Services.ServiceReply subRequest = await ubiiClient.Subscribe(topicTestSubscribe, (Ubii.TopicData.TopicDataRecord record) =>
         {
-            testPosition = new Vector3((float)record.Vector3.X, (float)record.Vector3.Y, (float)record.Vector3.Z);
+            //testPosition = new Vector3((float)record.Vector3.X, (float)record.Vector3.Y, (float)record.Vector3.Z);
+            //predictions = record.Object2DList;
+            x = record.Double;
         });
         testRunning = true;
     }
@@ -149,8 +172,8 @@ public class TestTensorflowCommunication : MonoBehaviour
         topicTestPublish = "/" + ubiiClient.GetID() + "/test_tensorflow/test_publish";
 
         ubiiDevice = new Ubii.Devices.Device { Name = deviceName, ClientId = ubiiClient.GetID(), DeviceType = Ubii.Devices.Device.Types.DeviceType.Participant };
-        ubiiDevice.Components.Add(new Ubii.Devices.Component { IoType = Ubii.Devices.Component.Types.IOType.Output, MessageFormat = "ubii.dataStructure.Vector3", Topic = topicTestPublish });
-        ubiiDevice.Components.Add(new Ubii.Devices.Component { IoType = Ubii.Devices.Component.Types.IOType.Input, MessageFormat = "ubii.dataStructure.Vector3", Topic = topicTestSubscribe });
+        ubiiDevice.Components.Add(new Ubii.Devices.Component { IoType = Ubii.Devices.Component.Types.IOType.Input, MessageFormat = "double", Topic = topicTestPublish });
+        ubiiDevice.Components.Add(new Ubii.Devices.Component { IoType = Ubii.Devices.Component.Types.IOType.Output, MessageFormat = "double", Topic = topicTestSubscribe });
        
     }
 
@@ -168,9 +191,9 @@ public class TestTensorflowCommunication : MonoBehaviour
         };
         ubiiSession.Interactions.Add(ubiiInteraction);
 
-        inputMapping = new Ubii.Sessions.InteractionInputMapping { Name = ubiiInteraction.InputFormats[0].InternalName, Topic = ubiiDevice.Components[0].Topic };
-        outputMapping = new Ubii.Sessions.InteractionOutputMapping { Name = ubiiInteraction.OutputFormats[0].InternalName, Topic = ubiiDevice.Components[1].Topic };
-        IOMapping = new Ubii.Sessions.IOMapping { InteractionId = interactionID };
+        inputMapping = new Ubii.Sessions.InteractionInputMapping { Name = "defaultIn", Topic = topicTestPublish };
+        outputMapping = new Ubii.Sessions.InteractionOutputMapping { Name = "defaultOut", Topic = topicTestSubscribe };
+        IOMapping = new Ubii.Sessions.IOMapping { InteractionId = this.ubiiInteraction.Id };
         IOMapping.InputMappings.Add(inputMapping);
         IOMapping.OutputMappings.Add(outputMapping);
         ubiiSession.IoMappings.Add(IOMapping);
