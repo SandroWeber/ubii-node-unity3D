1b9225c8d03735fd2475c82ea79e93597a33e309
leon <ga84teh@mytum.de>
2020-07-18 12:16:48 +0200

    started working on processingClient implementation;

---
+++ Ubi-Interact-Client/Assets/Scripts/ubii/client/UbiiProcessingClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Ubii.Services;
+using Ubii.TopicData;
+using Ubii.Interactions;
+using NetMQ;
+using NetMQ.Sockets;
+using Google.Protobuf;
+using UnityEngine;
+
+public class UbiiProcessingClient : MonoBehaviour
+{
+    private string host;
+    private int port;
+    RequestSocket serviceSocket;
+    DealerSocket topicdataSocket;
+    bool ssConnected = false;
+    bool tdsConnected = false;
+
+    private Dictionary<string, Action<TopicDataRecordList>> onProcessingCallbacks;
+    private Dictionary<string, Action<TopicDataRecordList>> onCreatedCallbacks;
+    private InteractionStatus status;
+}
