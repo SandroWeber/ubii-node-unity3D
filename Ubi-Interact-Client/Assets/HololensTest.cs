05433275c349f04c074f270c7dd7921923af34f4
ge25vif <ge25vif@ForStudents.onmicrosoft.com>
2024-07-29 19:17:06 +0200

    Configure scene to use MRTK, work in progress

--- Ubi-Interact-Client/Assets/HololensTest.cs
+++ Ubi-Interact-Client/Assets/HololensTest.cs
@@ -1,18 +1,34 @@
 using System.Collections;
-using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.UI;
 using UnityEngine;
 
+//This script will pulish some test data every time the Cube is grabbed or released
 public class HololensTest : MonoBehaviour
 {
+    [SerializeField]
+    private UbiiNode ubiiNode;
+
+    [SerializeField]
+    private ObjectManipulator objectManipulator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        objectManipulator.OnManipulationStarted.AddListener(OnManipulationStarted);
+        objectManipulator.OnManipulationEnded.AddListener(OnManipulationEnded);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnManipulationStarted(ManipulationEventData arg0)
     {
-        
+        StartCoroutine(PublishTestData("started"));
+    }
+
+    private void OnManipulationEnded(ManipulationEventData arg0)
+    {
+        StartCoroutine(PublishTestData("ended"));
+    }
+
+    private IEnumerator PublishTestData(string eventType){
+        yield return new WaitUntil(() => ubiiNode.WaitForConnection().IsCompleted);
+        ubiiNode.Publish(new Ubii.TopicData.TopicDataRecord { Topic = "HololensTest", String = "Cube Manipulation " + eventType });
     }
 }
